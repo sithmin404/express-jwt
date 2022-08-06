8800e3afef451bfee9794375aa1b56a726b730bf
sithminbandara <sithminbandara64@gmail.com>
2022-08-06 05:36:42 +0000

    Update usermodel.js - encrypt password

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -14,6 +14,12 @@ const userSchema = new mongoose.Schema({
         required:true,
         minlength:8
     }
+})
+
+userSchema.pre('save',async function(next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password,salt)
+    next()
 })
 
 const userModel = mongoose.model('user',userSchema)
